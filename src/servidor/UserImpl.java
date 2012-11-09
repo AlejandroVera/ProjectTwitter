05f04435a1f8d1a1b043da038e71fa58b970fb03
kmilinho <c.pereira.leon@gmail.com>
2012-11-09 11:34:20 +0000

    constructor de UserImpl con id

--- src/servidor/UserImpl.java
+++ src/servidor/UserImpl.java
@@ -10,13 +10,22 @@ public class UserImpl implements User{
 	private static final long serialVersionUID = -4749433293227574768L;
 
 	private String name;
+	private int id;
 	
+	//Constrcutor 1
 	public UserImpl(String name){
 		this.name = name;
+	}
+	//Constructor 2
+	public UserImpl(int id){
+		this.id = id;
 	}
 	
 	public String getName(){
 		return name;
 	}
 	
+	public int getId(){
+		return id;
+	}
 }
