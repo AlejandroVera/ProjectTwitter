64da9f95d2b527cfbcb8f332012a3877975e30e8
kmilinho <c.pereira.leon@gmail.com>
2012-11-10 00:44:44 +0000

    completada interface User

--- src/interfacesComunes/User.java
+++ src/interfacesComunes/User.java
@@ -6,7 +6,35 @@ import java.io.Serializable;
 
 public interface User extends Serializable{
 	
-	public String getName();
-	public String getLocation();
-	//TODO Camilo: faltan métodos!
+	boolean equals(java.lang.Object other);
+	java.util.Date 	getCreatedAt();
+	String getDescription();
+	int getFavoritesCount();
+	int getFollowersCount();
+	int getFriendsCount();
+	int getId();
+	String getLang();
+	String getLocation();
+	String getName();
+	Place getPlace();
+	String getProfileBackgroundColor();
+	java.net.URI getProfileBackgroundImageUrl();
+	java.net.URI getProfileImageUrl(); 
+	java.lang.String getProfileLinkColor();
+	java.lang.String getProfileSidebarBorderColor();
+	java.lang.String getProfileSidebarFillColor();
+	java.lang.String getProfileTextColor();
+	boolean 	getProtectedUser();
+	String getScreenName();
+	Status getStatus();
+	int getStatusesCount();
+	java.net.URI getWebsite();
+	boolean isFollowedByYou();
+	java.lang.Boolean isFollowingYou();//TODO: No faltaria un parametro User?
+	boolean isNotifications();
+	boolean isProfileBackgroundTile(); 
+	boolean isProtectedUser();
+	boolean isVerified();
+	java.lang.String toString();
+	
 }
