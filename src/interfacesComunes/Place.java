e0a9a9d512aeb5c9baa1d856b5a617a03fc407cb
billescas <mrbim18@gmail.com>
2012-11-10 21:07:14 +0000

    Cambiado lo de los puntos de la Bounding Box

--- src/interfacesComunes/Place.java
+++ src/interfacesComunes/Place.java
@@ -8,14 +8,14 @@ public interface Place extends Serializable {
     //public java.lang.String getCountryCode();
     public java.lang.String getCountryName();
     //public java.util.List<com.winterwell.jgeoplanet.Location> getGeometry();
-    public java.lang.String getId();
+    public int getId();
     //public java.lang.String getInfoUrl();
     public java.lang.String getName();
     public java.lang.String getType();
     public java.lang.String toString();
     //public com.winterwell.jgeoplanet.Location getCentroid();
-    public java.lang.String getUID();
-    public int[] getBoundingBox(); //Cuadro de coordenadas, la BoundingBox;
+   // public java.lang.String getUID();
+    public Point[] getBoundingBox(); //La Bounding Box del lugar, el cuadradito que ocupa
     
 	
 }
