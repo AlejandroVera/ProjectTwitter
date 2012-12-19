d03bb97fbdcfa95c3b7c6d5efb97a7e1a107d9ed
billescas <mrbim18@gmail.com>
2012-12-19 20:30:46 +0000

    Arreglado mi problema con lo de Desktop

--- src/cliente/LoginController.java
+++ src/cliente/LoginController.java
@@ -281,7 +281,49 @@ public class LoginController extends Controller{
 		/** no tengo el TOKEN, conectando por primera vez con twitter*/
 		OAuthSignpostClient oauthClient = new OAuthSignpostClient(JKEY, 
 				JSECRET, "oob");
-		oauthClient.authorizeDesktop();
+		if (java.awt.Desktop.isDesktopSupported()){
+			oauthClient.authorizeDesktop();
+		}
+		else{
+
+			String url =oauthClient.authorizeUrl().toString();
+			String os = System.getProperty("os.name").toLowerCase();
+			Runtime rt = Runtime.getRuntime();
+
+			try{
+
+				if (os.indexOf( "win" ) >= 0) {
+
+					// this doesn't support showing urls in the form of "page.html#nameLink" 
+					rt.exec( "rundll32 url.dll,FileProtocolHandler " + url);
+
+				} else if (os.indexOf( "mac" ) >= 0) {
+
+					rt.exec( "open " + url);
+
+				} else if (os.indexOf( "nix") >=0 || os.indexOf( "nux") >=0) {
+
+					// Do a best guess on unix until we get a platform independent way
+					// Build a list of browsers to try, in this order.
+					String[] browsers = {"epiphany", "firefox", "mozilla", "konqueror",
+							"netscape","opera","links","lynx"};
+
+					// Build a command string which looks like "browser1 "url" || browser2 "url" ||..."
+					StringBuffer cmd = new StringBuffer();
+					for (int i=0; i<browsers.length; i++)
+						cmd.append( (i==0  ? "" : " || " ) + browsers[i] +" \"" + url + "\" ");
+
+					rt.exec(new String[] { "sh", "-c", cmd.toString() });
+
+				} else {
+					return;
+				}
+			}catch (Exception e){
+				return;
+			}
+		}
+
+
 		String v = OAuthSignpostClient.askUser("Please enter the verification PIN from Twitter");
 		oauthClient.setAuthorizationCode(v);
 		String[] ats = oauthClient.getAccessToken();
